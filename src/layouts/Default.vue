6455a23f7862ca91d0af3b069bc69e4b37cb4824
jstalinko <alinkodotid@gmail.com>
2024-06-16 14:25:44 +0700

    hidden

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -32,7 +32,7 @@
                  
                     <slot/>
                 </div>
-                <div class="block md:none">
+                <div class="block md:hidden">
                   <svg class="absolute-bottom-0.5 w-full max-h-1.5" viewBox="0 0 55 5" xmlns="http://www.w3.org/2000/svg"
                   preserveAspectRatio="none">
                   <path d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002" strokeWidth="2"></path>
